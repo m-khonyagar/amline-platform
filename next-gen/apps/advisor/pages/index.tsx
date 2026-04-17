4a9a316de90268b8189d7d140e7791100bfd9001
m-khonyagar <158306758+m-khonyagar@users.noreply.github.com>
2026-04-17 20:43:49 +0330

    bootstrap v2: advisor dashboard shell

---
+++ next-gen/apps/advisor/pages/index.tsx
@@ -0,0 +1,8 @@
+export default function AdvisorDashboard() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>Advisor Dashboard</h1>
+      <p>پنل مشاوران (در حال توسعه)</p>
+    </div>
+  )
+}
