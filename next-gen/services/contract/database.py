78636212e82337f8b8a9a38bf82e81b7d518226d
m-khonyagar <158306758+m-khonyagar@users.noreply.github.com>
2026-04-17 21:37:56 +0330

    mvp: default to sqlite for easy local run

--- next-gen/services/contract/database.py
+++ next-gen/services/contract/database.py
@@ -2,8 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 import os
 
-DATABASE_URL = os.getenv('DATABASE_URL', 'postgresql://amline:amline@postgres:5432/amline')
+DATABASE_URL = os.getenv('DATABASE_URL', 'sqlite:///./amline.db')
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False} if DATABASE_URL.startswith('sqlite') else {})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
