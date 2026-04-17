56d0735bb73fd21426c2879112192d36e4457556
m-khonyagar <158306758+m-khonyagar@users.noreply.github.com>
2026-04-17 20:31:02 +0330

    bootstrap v2: people home page

---
+++ next-gen/apps/people/pages/index.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div style={{ padding: 40, fontFamily: 'sans-serif' }}>
+      <h1>Amline People App</h1>
+      <p>سیستم در حال اجرا است</p>
+      <a href="/login">ورود</a>
+    </div>
+  )
+}
