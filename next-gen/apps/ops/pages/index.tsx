4e4f9f9585dde8b99d4757a5eec01c5ce5c0676c
m-khonyagar <158306758+m-khonyagar@users.noreply.github.com>
2026-04-17 20:44:05 +0330

    bootstrap v2: ops dashboard shell

---
+++ next-gen/apps/ops/pages/index.tsx
@@ -0,0 +1,8 @@
+export default function OpsDashboard() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>Ops Dashboard</h1>
+      <p>پنل عملیات و کارشناسان (در حال توسعه)</p>
+    </div>
+  )
+}
